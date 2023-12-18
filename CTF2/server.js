ea171dd0597e4eba23dd6860e519697837337b63
IKaneI <154243191+IKaneI@users.noreply.github.com>
2023-12-19 02:50:37 +0300

    CTFXSS2

---
+++ CTF2/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/submit', (req, res) => {
+  let name = req.query.name;
+  let modifiedName = '';
+
+  for (let i = 0; i < name.length; i++) {
+    
+      if (name[i] === "'") {
+      modifiedName += "\\'"; // Add a backslash before a single quote
+    } else {
+      modifiedName += name[i];
+    }
+  }
+
+  // Append a <script> block with the modified input
+  let scriptBlock = `
+   <script> var yourInput = '${modifiedName}'; </script>`;
+                    
+                       
+
+  res.send(`<input type="text" id="name" name="name">\n${scriptBlock}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
